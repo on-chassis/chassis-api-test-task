8d63e65f0e6e2aea2faea2b4336f5c590085ee28
Timothy Sharakh <shati@shatinik.me>
2022-09-28 01:05:45 +0300

    Poll must have at least one section. Section must have at least one question

--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -18,11 +18,13 @@ export class PollsService {
     poll.setCreator(creator);
     poll.sections = [];
     poll.title = createPollDto.title;
+    if (!createPollDto.sections.length) throw new Error('Poll must have at least one section')
     for (let i = 0; i < createPollDto.sections.length; i++) {
       const section = new PollSection();
       section.orderNumber = i;
       section.title = createPollDto.sections[i].title;
       section.questions = [];
+      if (!createPollDto.sections[i].questions.length) throw new Error('Section must have at least one question')
       for (let j = 0; j < createPollDto.sections[i].questions.length; j++) {
         const question = new PollQuestion();
         question.orderNumber = j;
