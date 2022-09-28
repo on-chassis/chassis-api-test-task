603709046412560e61d5f636fc62206604b08f04
Timothy Sharakh <shati@shatinik.me>
2022-09-28 21:00:42 +0300

    Remove hello world

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PollsModule } from './polls/polls.module';
@@ -30,7 +28,6 @@ import { randomUUID } from 'crypto';
     UsersModule,
     PollsModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
